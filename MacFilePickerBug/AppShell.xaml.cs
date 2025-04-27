08f8c896eec03dc75e5833268175bdb2c3b77b11
James Crutchley <ne0rmatrix@gmail.com>
2025-04-26 18:02:15 -0700

    Add project files.

---
+++ MacFilePickerBug/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MacFilePickerBug
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MacFilePickerBug/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace MacFilePickerBug
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OpenFilePicker(object sender, EventArgs e)
+        {
+            var result = await FilePicker.PickAsync(new PickOptions
+            {
+                PickerTitle = "Please select a file",
+                FileTypes = FilePickerFileType.Images
+            });
+            if (result is not null)
+            {
+                var file = result.FileName;
+                if (file is not null)
+                {
+                    await DisplayAlert("File Selected", file, "OK");
+                }
+            }
+        }
+    }
+
+}
